baf23e608958ad41c135674b8a0e6c119af740a5
seungok <tkqkfausp@gmail.com>
2021-01-22 13:53:00 +0900

    test4

---
+++ src/main/java/com/example/demo/WebRestController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import com.example.demo.domain.posts.PostsRepository;
+import com.example.demo.dto.posts.PostsSaveRequestDto;
+import com.example.demo.service.PostsService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsService postsService;
+
+    private PostsRepository postsRepository;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
+        return postsService.save(dto);
+    }
+
+}
